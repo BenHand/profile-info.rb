28ead9ba6e05796cbcdc454cc94189e39d2bac49
Benjamin Hand <gneeshot@gmail.com>
2015-05-11 15:57:14 -0500

    no method yet. done otherwise

--- profile-info.rb
+++ profile-info.rb
@@ -1,20 +1,21 @@
+
 fav_food = ['Pizza', 'Gyros', 'Chips & Queso', 'Banana Bread', 'Potato Soup']
 name = "Benjamin Hand"
 age = 27
+username = "Gneeshot"
 from = { city: "Spring", state: 'Texas', country: 'USA'}
 prompt = ">> "
 
 # def print_profile
-#   puts "My name is #{name}, i am #{age} years old and i am from #{from.values.join(", ")}, and i enjoy the following foods: #{fav_food.join(", ")}"
+  # puts "My name is #{name} but you may know me as #{username}, i am #{age} years old and i am from #{from.values.join(", ")}, and i enjoy the following foods: #{fav_food.join(", ")}!"
 # end
 
 
-
-print "Would you like to review the above information? #{prompt}"
+print "Would you like to know some very sensitive information about me? #{prompt}"
 response = gets.chomp
 if response == "yes"
   # print_profile
-  puts "My name is #{name}, i am #{age} years old and i am from #{from.values.join(", ")}, and i enjoy the following foods: #{fav_food.join(", ")}!"
+  puts "My name is #{name} but you may know me as #{username}, i am #{age} years old and i am from #{from.values.join(", ")}, and i enjoy the following foods: #{fav_food.join(", ")}!"
 
 
 else
