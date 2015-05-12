080f2ddf26d4f6b4db706f8796c8245896b50386
Benjamin Hand <gneeshot@gmail.com>
2015-05-11 19:06:34 -0500

    Nightmare Mode complete

--- profile-info.rb
+++ profile-info.rb
@@ -1,22 +1,22 @@
 
-fav_food = ['Pizza', 'Gyros', 'Chips & Queso', 'Banana Bread', 'Potato Soup']
-name = "Benjamin Hand"
-age = 27
-username = "Gneeshot"
-from = { city: "Spring", state: 'Texas', country: 'USA'}
-prompt = ">> "
+$fav_food = ['Pizza', 'Gyros', 'Chips & Queso', 'Banana Bread', 'Potato Soup']
+$name = "Benjamin Hand"
+$age = 27
+$username = "Gneeshot"
+$from = { city: "Spring", state: 'Texas', country: 'USA'}
+$prompt = ">> "
 
 
-# def print_profile
-#    puts "My name is #{name} but you may know me as #{username}, i am #{age} years old and i am from #{from.values.join(", ")}, and i enjoy the following foods: #{fav_food.join(", ")}!"
-# end
+def print_profile
+   puts "My name is #{$name} but you may know me as #{$username}, i am #{$age} years old and i am from #{$from.values.join(", ")}.\nThe following are a few of my favorite foods: #{$fav_food.join(", ")}!"
+end
 
 
-print "Would you like to know some very sensitive information about me? #{prompt}"
+print "Would you like to know some very sensitive information about me? #{$prompt}"
 response = gets.chomp
 if response == "yes"
-  # print_profile
-  puts "My name is #{name} but you may know me as #{username}, i am #{age} years old and i am from #{from.values.join(", ")}, and i enjoy the following foods: #{fav_food.join(", ")}!"
+  print_profile
+  # puts "My name is #{name} but you may know me as #{username}, i am #{age} years old and i am from #{from.values.join(", ")}, and i enjoy the following foods: #{fav_food.join(", ")}!"
 
 
 else
